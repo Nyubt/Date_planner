604c2014b5ad1c45ef068fc1f4360173d8522a83
Elena Ciumeica <e.w.child@gmail.com>
2021-06-14 15:06:50 +0300

    generated spring initializr

---
+++ dateplanner/src/main/java/com/mycompany/dateplanner/DateplannerApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.dateplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateplannerApplication.class, args);
+	}
+
+}
