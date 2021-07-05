b5344d63928ba7397f3daecc092e0e8a4cb2b599
Elena Ciumeica <e.w.child@gmail.com>
2021-07-05 14:50:58 +0300

    Create user and product services
    - Connected to db
    - Created User and Product classes
    - Created Product and User Services
    - Created Product and User Repository Interfaces

---
+++ dateplanner/src/main/java/com/mycompany/dateplanner/model/Product.java
@@ -0,0 +1,66 @@
+package com.mycompany.dateplanner.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class Product implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    @Column(nullable = false, updatable = false)
+    private Long userId;
+    private String title;
+    private String description;
+    private Float price;
+
+    public Product() {
+    }
+
+    public Product(String title, String description, Float price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+}
---
+++ dateplanner/src/main/java/com/mycompany/dateplanner/service/ProductService.java
@@ -0,0 +1,42 @@
+package com.mycompany.dateplanner.service;
+
+import com.mycompany.dateplanner.exception.RecordNotFoundException;
+import com.mycompany.dateplanner.model.Product;
+import com.mycompany.dateplanner.model.User;
+import com.mycompany.dateplanner.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+    private final ProductRepo productRepo;
+
+    @Autowired
+    public ProductService(ProductRepo productRepo) {
+        this.productRepo = productRepo;
+    }
+
+    public Product addProduct(Product product, User user) {
+        product.setUserId(user.getId());
+        return productRepo.save(product);
+    }
+
+    public Product updateProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(Long id) {
+        productRepo.deleteProductById(id);
+    }
+
+    public List<Product> findAllUsers() {
+        return productRepo.findAll();
+    }
+
+    public Product findProductById(Long id) {
+        return productRepo.findProductById(id)
+                .orElseThrow(() -> new RecordNotFoundException("Product by id " + id + " not found"));
+    }
+}
